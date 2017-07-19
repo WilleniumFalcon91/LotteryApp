05597ef993bf6e7f1652165b42b2c13bce788258
William Elliott <wcelliott1991@gmail.com>
2017-07-19 15:10:47 -0400

    creates parameters of unique and random numbers for the two distinct lottery number sections

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,35 @@
 function firstNumberSet() {
-    var result = [];
-    for (var i = 0; i < 5; i++) {
-        result.push(Math.floor((Math.random() * 69) + 1));
-    }
-    return result;
+    var nums = {};
+    
+    while (Object.keys(nums).length < 5) {
+      var rando = randomFirstNumber();
+      nums[rando] = true;
+    } 
+    console.log(Object.keys(nums));
+    
+}
+
+function secondNumberSet() {
+    var nums = {};
+    
+    while (Object.keys(nums).length < 2) {
+      var rando = randomSecondNumber();
+      nums[rando] = true;
+    } 
+    console.log(Object.keys(nums));
+}
+
+
+
+function randomFirstNumber(){
+  var result = (Math.floor((Math.random() * 69) + 1));
+  return result;
+}
+
+function randomSecondNumber(){
+  var result = (Math.floor((Math.random() * 26) + 1));
+  return result;
 }
 
 firstNumberSet();
+secondNumberSet();
